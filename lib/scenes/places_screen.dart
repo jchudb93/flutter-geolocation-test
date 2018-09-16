e29d9e167318f719d2bfebac8b75fd1d4c0b0c29
José Chu <jchudb93@gmail.com>
2018-09-15 20:41:35 -0500

    List widget

--- lib/scenes/places_screen.dart
+++ lib/scenes/places_screen.dart
@@ -1,36 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_geolocation_test/services/place_service.dart';
 
 class PlacesScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    
     return new PlacesScreenState();
   }
-
 }
-
 
 class PlacesScreenState extends State<PlacesScreen> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Nearby Places");
-      )
-
-      body: new ListView(
-        children: <Widget>[
-          new  ListTile(
-            title: new Text("First Item"),
-            trailing: new Icon(Icons.arrow_forward)
-          ),
-
+        appBar: new AppBar(title: new Text("Nearby Places")),
+        body: new ListView(children: <Widget>[
           new ListTile(
-            title: new Text("Second item"),
-            trailing: new Icon(Icons.arrow_forward)
-          )
-        ]
-      )
-    )
-  } 
+              title: new Text("First Item"),
+              trailing: new Icon(Icons.arrow_forward)),
+          new ListTile(
+              title: new Text("Second item"),
+              trailing: new Icon(Icons.arrow_forward))
+        ]));
+  }
+List <Place> _places;
+  @override void initState(){
+    super.initState();
+
+    PlacesService.get().getNearbyPlaces().then((data) {
+      this.setState((){
+        _places = data;
+      });
+    });
+  }
 }
--- lib/services/place_service.dart
+++ lib/services/place_service.dart
@@ -2,26 +2,27 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-import 'package:flutter_geolocation_test/modules/place_model.dart';class PlaceService{
+import 'package:flutter_geolocation_test/modules/place_model.dart';
 
-  static final _service = new PlaceService();
+class PlacesService {
+  static final _service = new PlacesService();
 
-  static PlaceService get() {
-
+  static PlacesService get() {
     return _service;
   }
 
-  final String searchUrl ='';
+  final String searchUrl = '';
 
-  Future<List<Place>> getNearbyPlace() async {
-    var response = await http.get(searchUrl, headers:{"Accept":"application/json"});
+  Future<List<Place>> getNearbyPlaces() async {
+    var response =
+        await http.get(searchUrl, headers: {"Accept": "application/json"});
     var places = <Place>[];
 
     List data = json.decode(response.body)['results'];
 
-    data.forEach((f) => places.add(new Place(f['icon'], f['name'], f['rating'].toString(), f['vicinity'], f['place_id'])));
+    data.forEach((f) => places.add(new Place(f['icon'], f['name'],
+        f['rating'].toString(), f['vicinity'], f['place_id'])));
 
     return places;
   }
-
 }
