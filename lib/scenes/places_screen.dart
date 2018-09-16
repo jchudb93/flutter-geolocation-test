cd126152ed1e2c3fed689ede2a345a2e7863709e
José Chu <jchudb93@gmail.com>
2018-09-15 20:50:42 -0500

    places

--- lib/scenes/places_screen.dart
+++ lib/scenes/places_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_geolocation_test/modules/place_model.dart';
 import 'package:flutter_geolocation_test/services/place_service.dart';
 
 class PlacesScreen extends StatefulWidget {
@@ -13,21 +14,35 @@ class PlacesScreenState extends State<PlacesScreen> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text("Nearby Places")),
-        body: new ListView(children: <Widget>[
-          new ListTile(
-              title: new Text("First Item"),
-              trailing: new Icon(Icons.arrow_forward)),
-          new ListTile(
-              title: new Text("Second item"),
-              trailing: new Icon(Icons.arrow_forward))
-        ]));
+        body: _createContent(),
+        );
   }
-List <Place> _places;
-  @override void initState(){
+
+  Widget _createContent(){
+    if(_places == null){
+      return new Center(
+        child: new CircularProgressIndicator(),
+      );
+    } else {
+      return new ListView(
+        children: _places.map((f){
+          return new ListTile(
+            title: new Text(f.name),
+            leading: new Image.network(f.icon),
+            subtitle: new Text(f.vicinity),
+          );
+        }).toList(),
+      );
+    }
+  }
+
+  List<Place> _places;
+  @override
+  void initState() {
     super.initState();
 
     PlacesService.get().getNearbyPlaces().then((data) {
-      this.setState((){
+      this.setState(() {
         _places = data;
       });
     });
